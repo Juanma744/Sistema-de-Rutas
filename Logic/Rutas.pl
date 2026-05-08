12b91126566e52bd6cca5cf4fd3249cf88b535ba
Juanma744 <juanmahi1@hotmail.com>
2026-05-07 20:52:18 -0600

    x

---
+++ Logic/Rutas.pl
@@ -0,0 +1,6 @@
+% Todo lo que inicie con mayuscula son variables, lo que inicie con minuscula es un valor constante
+
+lugar(zamora).
+lugar(morelia).
+%        origen, destino, distancia, tiempo, tipo de camino 
+conexion(zamora, morelia, 144, 200, cuota).
